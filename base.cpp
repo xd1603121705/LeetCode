6977adf85f6db5304423c4c5e15f05dc07ef716c
PC-PC\PC <junlee_happy@163.com>
2017-09-28 11:17:51 +0800

    617: Merge Two Binary Trees

---
+++ base.cpp
@@ -0,0 +1,27 @@
+// Source : https://leetcode.com/problems/partition-list/description/
+// Author : Li Junjun
+// Date   : 2017/09/18
+
+/**********************************************************************************
+*
+*       title: 86. Partition List
+*
+*     problem: problem introduce
+*
+*    solution: solution introduce
+*
+* description: 程序输入输出重定向到了文件中，输入从"data.in"中读取，输出读入到
+*              "data.out"中。
+*              输入：第一行为结点数目N（输入字符退出），第二行为N个整数。
+*              举例：5
+*                    5 4 3 2 1
+*                    10
+*                    6 3 5 7 9 2 1 4 8 0
+*                    q
+*              输出：original list is: 5->4->3->2->1
+*                    swap list: 2->1->5->4->3
+*                    original list is: 6->3->5->7->9->2->1->4->8->0
+*                    swap list: 2->1->0->6->3->5->7->9->4->8
+*
+**********************************************************************************/
+
